2dfdb2148d82a3e0288ff4451a3578887a01d6fa
tizzytzy <Sama.tzy@outlook.com>
2018-04-15 15:25:29 +0800

    类的封装及测试

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Water water = new Water();
+        water.setBrand("百岁山");
+        System.out.println(water.getBrand());
+
+        Milk milk = new Milk();
+        milk.setLocality("南京");
+        System.out.println(milk.getLocality());
+
+
+        TreadMill treadMill = new TreadMill();
+        treadMill.setBrand("金吉鸟");
+        System.out.println(treadMill.getBrand());
+
+        }
     }
-}
--- src/TreadMill.java
+++ src/TreadMill.java
@@ -23,4 +23,72 @@ public class TreadMill {
     // 上架时间
     private Date publishTime;
 
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getHeartFitness() {
+        return heartFitness;
+    }
+
+    public void setHeartFitness(String heartFitness) {
+        this.heartFitness = heartFitness;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getDriveMode() {
+        return driveMode;
+    }
+
+    public void setDriveMode(String driveMode) {
+        this.driveMode = driveMode;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Date getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(Date publishTime) {
+        this.publishTime = publishTime;
+    }
+
+
+
 }
